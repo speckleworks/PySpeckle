cb19bce75547c5a86166fe7111c17ad0cf61b1c8
AntoineDao <antoinedao1@gmail.com>
2019-06-19 20:20:03 +0100

    ci(semantic release): setup semantic release
    This will trigger build and deployments to PyPi based on commit messages. For more information
    checkout this: https://blog.greenkeeper.io/introduction-to-semantic-release-33f73b117c8. I also
    recommend installing comittizen: https://github.com/commitizen/cz-cli

---
+++ setup.py
@@ -0,0 +1,26 @@
+import re
+import setuptools
+import sys
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="speckle",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Speckle Works",
+    description="A Python client for Speckle servers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/specklworks.pyspeckle",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    install_requires=required,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent"
+    ],
+)
