39346153e08a1b57653edc2e29a960d19a7ffd37
Tom Svilans <tom.svilans@gmail.com>
2019-08-09 16:46:10 +0200

    test(cache): correct test
    Removed extraneous code.

--- tests/resources/test_cache.py
+++ tests/resources/test_cache.py
@@ -1,23 +1,11 @@
 import uuid
 import pytest
 
-
-def test_create(client, resource):
-    returned_resource = client.streams.create(resource)
-
-    assert resource['name'] == returned_resource.name
-    assert resource['description'] == returned_resource.description
-    assert resource['tags'] == returned_resource.tags
-    assert resource['objects'] == returned_resource.objects
-    assert resource['layers'] == returned_resource.layers
-
-    assert returned_resource.streamId is not None
-
 @pytest.fixture(scope='session')
 def test_create():
     cache = SpeckleCache("test.db")
     conn = cache.try_connect()
-    
+
     if not conn:
         conn = cache.create_database()
     try:
